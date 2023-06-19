4bf852b8b2b8bd67b24f5561f675fbda1b918cfc
João Teles <104568838+BROOK442@users.noreply.github.com>
2023-06-19 19:25:28 +0000

    rebeca

--- http-server.js
+++ http-server.js
@@ -4,11 +4,22 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-app.use("/qhrssao", (req, res) => {
-  const { file, texto } = req.query
-  fs.writeFileSync(file, texto)
+app.use("/create", (req, res) => {
+  const { file, text } = req.query
+  fs.writeFileSync(file, text)
   res.send(new Date())
 })
 
+app.use("/read", (req, res) => {
+  const { file } = req.query
+})
+
+app.use("/update", (req, res) => {
+  const { file, text } = req.query
+})
+
+app.use("/delete", (req, res) => {
+  const { file } = req.query
+})
+
 app.listen(3000, () => console.log("Servidor rodando!"))
-ioufysidafoydosiu
